8f3f6f00d8f8bdcf638567bf3b3118b67546cfc3
n4ng1h <negoh@visa.com>
2021-10-07 13:46:40 +0800

    Feat: Added error message support for the CustomTextField.

--- src/scenes/Main/index.jsx
+++ src/scenes/Main/index.jsx
@@ -5,12 +5,30 @@ import CustomTextField from 'components/CustomTextField';
 import { Grid } from '@material-ui/core';
 import Content from 'content';
 import BUTTON_TYPE from 'constant';
+import isKingdomNameValid from 'services/validation';
 import useStyles from './styles';
 
 const MainPage = () => {
   const classes = useStyles();
   // eslint-disable-next-line no-unused-vars
   const [kingdomName, setKingdomName] = useState('');
+  // eslint-disable-next-line no-unused-vars
+  const [errorMsg, setErrorMsg] = useState('');
+  const resetErrorMsg = () => {
+    setErrorMsg('');
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  const handleStartGame = () => {
+    if (isKingdomNameValid(kingdomName)) {
+      resetErrorMsg();
+      // If it is valid, we proceed to the next step
+      // API call to submit user details to backend
+    } else {
+      // Display error to the user
+      setErrorMsg(Content.invalidKingdomName);
+    }
+  };
 
   return (
     <div>
@@ -32,10 +50,11 @@ const MainPage = () => {
               setKingdomName(e.target.value);
             }}
             maxLength={20}
+            errorMsg={errorMsg}
           />
         </Grid>
         <Grid item className={classes.item}>
-          <CustomButton btnType={BUTTON_TYPE.NEXT}>
+          <CustomButton btnType={BUTTON_TYPE.NEXT} onClick={handleStartGame}>
             {Content.startBtn}
           </CustomButton>
         </Grid>
