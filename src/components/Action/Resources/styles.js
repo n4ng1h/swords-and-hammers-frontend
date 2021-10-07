4a8e266b438a9cde55fad716c5601fc0f0d31222
n4ng1h <negoh@visa.com>
2021-10-08 00:45:37 +0800

    Created an ActionResources component which displays the resources in the Action component.

---
+++ src/components/Action/Resources/styles.js
@@ -0,0 +1,34 @@
+const styles = {
+  container: {
+    marginLeft: '15px',
+    height: 60,
+    width: '166px',
+    backgroundColor: 'white',
+    borderRadius: '0px 0px 15px 15px',
+  },
+  gridContainer: {
+    paddingTop: '5px',
+  },
+  imgLumber: {
+    width: 30,
+    height: 15,
+    paddingTop: 5,
+    marginBottom: 10,
+  },
+  imgSteel: {
+    width: 30,
+    height: 30,
+  },
+  imgGold: {
+    width: 30,
+    height: 30,
+  },
+  text: {
+    margin: 0,
+  },
+  noCostText: {
+    paddingTop: '15px',
+  },
+};
+
+export default styles;
