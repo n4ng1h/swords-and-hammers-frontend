faca19b8c6c8d13d4047709a7b55c7d48f8266a0
n4ng1h <negoh@visa.com>
2021-10-08 00:42:16 +0800

    Feat: Created the WarRoomPage component which encapsulates the relevant rows for the page. Still missing some.

---
+++ src/scenes/WarRoom/index.jsx
@@ -0,0 +1,31 @@
+import { Container, Grid } from '@mui/material';
+import HeaderRow from 'scenes/WarRoom/components/HeaderRow';
+import KingdomTitle from 'components/KingdomTitle';
+import ActionRow from 'scenes/WarRoom/components/ActionRow';
+import styles from './styles';
+
+const WarRoomPage = () => {
+  return (
+    <Container>
+      <Grid
+        container
+        direction="column"
+        justifyContent="flex-start"
+        alignItems="center"
+        spacing={4}
+      >
+        <Grid item sx={styles.statsControls}>
+          <HeaderRow />
+        </Grid>
+        <Grid item>
+          <KingdomTitle />
+        </Grid>
+        <Grid item>
+          <ActionRow />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default WarRoomPage;
---
+++ src/scenes/WarRoom/styles.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import SkyImage from 'assets/images/background/sky.png';
+import MountainImage from 'assets/images/background/mountain.png';
+import CastleImage from 'assets/images/background/castle.png';
+
+const styles = {
+  background: {
+    backgroundImage: `url(${SkyImage})`,
+    backgroundSize: 'cover',
+  },
+  statsControls: {
+    marginTop: 1,
+  },
+  actionControls: {
+    marginTop: 3,
+  },
+};
+
+export default styles;
