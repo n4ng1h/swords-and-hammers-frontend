75dc6673647203baf0fbe4c4caffd0e19b42c934
n4ng1h <negoh@visa.com>
2021-10-08 00:40:42 +0800

    Feat: Added the background image and opacity styles.

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import routes from './routes';
 function App() {
   return (
     <div className="App">
-      <Switch>
-        {routes.map((routeObj) => (
-          <Route key={routeObj.path} exact path={routeObj.path}>
-            {routeObj.component}
-          </Route>
-        ))}
-      </Switch>
+      <div className="Background">
+        <Switch>
+          {routes.map((routeObj) => (
+            <Route key={routeObj.path} exact path={routeObj.path}>
+              {routeObj.component}
+            </Route>
+          ))}
+        </Switch>
+      </div>
     </div>
   );
 }
