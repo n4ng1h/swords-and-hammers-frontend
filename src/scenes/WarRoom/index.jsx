bcc74753e6c4029b9bd1b45e70cce62616a1ab69
n4ng1h <negoh@visa.com>
2021-10-08 23:20:16 +0800

    Feat: Added the initial state for the available raw material resources for the user. i.e. Lumber, steel, gold.

--- src/scenes/WarRoom/index.jsx
+++ src/scenes/WarRoom/index.jsx
@@ -1,13 +1,26 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react';
 import { Container, Grid } from '@mui/material';
 import HeaderRow from 'scenes/WarRoom/components/HeaderRow';
 import KingdomTitle from 'components/KingdomTitle';
 import ResourceRow from 'scenes/WarRoom/components/ResourceRow';
 import EventLog from 'scenes/WarRoom/components/EventLog';
 import ActionRow from 'scenes/WarRoom/components/ActionRow';
+import { RAW_MATERIALS_TEMPLATE, BUILT_RESOURCES_TEMPLATE } from 'constant';
 import styles from './styles';
 
 // TODO: Create a state to listen for and update the number of user owned resources
 const WarRoomPage = () => {
+  const [rawMaterials, setRawMaterials] = useState(RAW_MATERIALS_TEMPLATE);
+  const [builtResources, setBuiltResources] = useState(
+    BUILT_RESOURCES_TEMPLATE
+  );
+
+  // Probably a subscription to listen for changes to the user's resources
+  useEffect(() => {
+    // And a cleanup function to unsubscribe
+  }, []);
+
   return (
     <Container>
       <Grid
@@ -24,13 +37,13 @@ const WarRoomPage = () => {
           <KingdomTitle />
         </Grid>
         <Grid item>
-          <ResourceRow />
+          <ResourceRow resourceOwned={rawMaterials} />
         </Grid>
         <Grid item>
           <EventLog />
         </Grid>
         <Grid item sx={styles.actionControls}>
-          <ActionRow />
+          <ActionRow resources={builtResources} />
         </Grid>
       </Grid>
     </Container>
