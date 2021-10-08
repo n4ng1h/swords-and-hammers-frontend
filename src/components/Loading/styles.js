523e727a88b8545d30b38bcde3bea9cef5695791
n4ng1h <negoh@visa.com>
2021-10-09 00:33:04 +0800

    Feat: Created a Loading component to display to the user.

---
+++ src/components/Loading/styles.js
@@ -0,0 +1,21 @@
+const styles = {
+  backdrop: {
+    zIndex: 999,
+  },
+  blinkDot: {
+    float: 'left',
+    width: 20,
+    height: 20,
+    bgcolor: 'white',
+    marginLeft: 1,
+    marginRight: 1,
+  },
+  gridItem: {
+    textAlign: 'center',
+  },
+  text: {
+    color: 'white',
+  },
+};
+
+export default styles;
