740c4143f7ebb3fcdd0784322ac1c57bc5999d86
n4ng1h <negoh@visa.com>
2021-10-10 03:41:56 +0800

    Feat: Added the end turn dialog related components and helper methods.

--- src/scenes/WarRoom/index.jsx
+++ src/scenes/WarRoom/index.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import { useEffect, useState } from 'react';
 import { Container, Grid } from '@mui/material';
 import HeaderRow from 'scenes/WarRoom/components/HeaderRow';
@@ -6,14 +5,35 @@ import KingdomTitle from 'components/KingdomTitle';
 import ResourceRow from 'scenes/WarRoom/components/ResourceRow';
 import EventLog from 'scenes/WarRoom/components/EventLog';
 import ActionRow from 'scenes/WarRoom/components/ActionRow';
-import { RAW_MATERIALS_TEMPLATE, BUILT_RESOURCES_TEMPLATE } from 'constant';
+import TurnEndDialog from 'scenes/WarRoom/components/TurnEndDialog';
+import {
+  RAW_MATERIALS_TEMPLATE,
+  BUILT_RESOURCES_TEMPLATE,
+  END_TURN_RESOURCES_TEMPLATE,
+} from 'constant';
 import styles from './styles';
 
 // TODO: Create a state to listen for and update the number of user owned resources
 const WarRoomPage = () => {
+  // eslint-disable-next-line no-unused-vars
   const [rawMaterials, setRawMaterials] = useState(RAW_MATERIALS_TEMPLATE);
+  // eslint-disable-next-line no-unused-vars
   const [builtResources, setBuiltResources] = useState(
     BUILT_RESOURCES_TEMPLATE
+  );
+  const [isTurnEndDialogOpen, setTurnEndDialogOpen] = useState(true);
+  // TODO: Open the turn end dialog when the end turn is detected
+  // eslint-disable-next-line no-unused-vars
+  const handleOpenTurnEndDialog = () => {
+    setTurnEndDialogOpen(true);
+  };
+  const handleCloseTurnEndDialog = () => {
+    setTurnEndDialogOpen(false);
+  };
+  // TODO: Update the number of end of turn resources
+  // eslint-disable-next-line no-unused-vars
+  const [endTurnResources, setEndTurnResources] = useState(
+    END_TURN_RESOURCES_TEMPLATE
   );
 
   // Probably a subscription to listen for changes to the user's resources
@@ -23,6 +43,11 @@ const WarRoomPage = () => {
 
   return (
     <Container>
+      <TurnEndDialog
+        open={isTurnEndDialogOpen}
+        closeDialog={handleCloseTurnEndDialog}
+        endTurnGain={endTurnResources}
+      />
       <Grid
         container
         direction="column"
