004884ff8ef10b6de80929040f362ce0404b6254
n4ng1h <negoh@visa.com>
2021-10-10 20:39:09 +0800

    Feat: Included the API action calls to perform the actions for building a village, army, castle, and attacking.

--- src/scenes/WarRoom/components/ActionRow/BuildVillage/index.jsx
+++ src/scenes/WarRoom/components/ActionRow/BuildVillage/index.jsx
@@ -1,16 +1,35 @@
 import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import SocketContext from 'contexts/Socket';
+import ResourceContext from 'contexts/Resource';
 import Action from 'components/Action';
 import VillageImage from 'assets/images/buttons/village.png';
 import Content from 'content';
+import { takeTurn, refreshResourceState, fetchEventLogs } from 'services/api';
+import { ACTION_TYPE } from 'constant';
 
-// TODO: Add an onClick function to tell the backend to build a village
 const BuildVillage = ({ numOwned }) => {
+  const { gameId } = useContext(SocketContext);
+  const { setResourceInfo, setEventLog } = useContext(ResourceContext);
+
+  const buildAVillage = async () => {
+    const buildOutcome = await takeTurn(gameId, ACTION_TYPE.BUILD_VILLAGE);
+    if (buildOutcome) {
+      // Update the resource state
+      const currResourceState = await refreshResourceState(gameId);
+      setResourceInfo(currResourceState);
+      // Refresh the event log
+      setEventLog(await fetchEventLogs(gameId));
+    }
+  };
+
   return (
     <Action
       btnImg={VillageImage}
       btnCaption={Content.action.buildVillage}
       resourceCost={{ lumber: -3, iron: -2, gold: 5 }}
       numOwned={numOwned}
+      onClick={buildAVillage}
     />
   );
 };
