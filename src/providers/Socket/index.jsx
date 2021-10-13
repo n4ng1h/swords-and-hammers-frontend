6dae899d2b19b191b5b2a6ac1eebbbc069213667
n4ng1h <negoh@visa.com>
2021-10-13 18:09:51 +0800

    Refactor: Resolving merge conflicts and cleanup.

--- src/providers/Socket/index.jsx
+++ src/providers/Socket/index.jsx
@@ -142,7 +142,7 @@ const SocketProvider = ({ children }) => {
 
     // Cleanup to ensure that we disconnect from the socket
     return () => {
-      socket.disconnect();
+      socket.socket.disconnect();
     };
   }, [gameData, history, socket]);
 
